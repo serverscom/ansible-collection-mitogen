d8efda796cb5b6d14c87ef049fec817885f5a9c5
George Shuklin <george.shuklin@gmail.com>
2023-03-13 15:57:58 +0200

    fix: giving up on ansible-test

--- plugins/strategy/mitogen_linear.py
+++ plugins/strategy/mitogen_linear.py
@@ -15,22 +15,6 @@ description: Ansible strategy plugin wrapper to patch mitogen version requiremen
 short_description: Mitogen patching strategy
 """
 
-# Double licensing: You can either GPLv3 or MIT licence.
-
-# GPLv3 license header
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 from . import patching
 
 with patching.patch_version():
